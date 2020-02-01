03dc23fde969c2a446f2ec77f6ec0684e9bea1c9
MateuVieira <mateus12v@hotmail.com>
2020-02-01 10:04:38 -0300

    Add nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6b03ba8c6ac062',
+    pass: '0fd68d88e0d0ef',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com',
+  },
+};
